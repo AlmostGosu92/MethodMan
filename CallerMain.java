f1595b8d5e24ab6081759bf54d3aef2c5991b5c8
Axel Eriksson <a.eriksson0303@gmail.com>
2019-11-07 10:44:31 +0100

    cleaned up syntax

--- CallerMain.java
+++ CallerMain.java
@@ -7,7 +7,7 @@ public class CallerMain {
 	public static void main(String[] args) {
 		MethodMan methodMan = new MethodMan();
 		boolean loop = true;
-		while(loop == true) {
+		while(loop) {
 			
 			String in = JOptionPane.showInputDialog("Welcome to the Method Man quote supplier!\nChoose a quote nr from 1 to 10 or leave blank for a random quote:");
 			
@@ -16,13 +16,14 @@ public class CallerMain {
 					methodMan.randomMethodMan();
 				}
 				
-				else if (in != null || in.length()>0) {
+				else if (in.length()>0) {
 					int i = Integer.parseInt(in);
 					methodMan.selectedMethodMan(i);
 				}
 			
-			}else
+			}else {
 				loop = false;
+			}
 		}
 		}
 }
